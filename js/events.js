21d7cd8b89c08883089d8c2c508711bc92fb9336
Anri Kawahara <anri0806@hotmail.co.jp>
2021-06-04 15:29:11 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -24,8 +24,7 @@ $('form').on('submit', function() {
   if ($('input:first').val() === 'correct') {
     alert('Your form is going to be submitted now.');
   }
-}
-)
+})
 }
 
 $(document).ready(function(){
